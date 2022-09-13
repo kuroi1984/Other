508f296bc0fa910a0054e550f531cc88747d7ae6
kot <hanza@mail.ru>
2022-09-13 14:38:40 +0300

    v2

--- Weather.py
+++ Weather.py
@@ -6,8 +6,8 @@ from pyowm.utils.config import get_default_config
 config_dict = get_default_config()
 config_dict['language'] = 'ru'  # your language here, eg. French
 
-#owm = OWM('823c1825a5d89946290e0a1d11172f2c')
-owm = OWM('6d00d1d4e704068d70191bad2673e0cc',config_dict)
+owm = OWM('823c1825a5d89946290e0a1d11172f2c',config_dict)
+#owm = OWM('6d00d1d4e704068d70191bad2673e0cc',config_dict)
 mgr = owm.weather_manager()
 
 observation = mgr.weather_at_place('Чебоксары')
---
+++ test.py
@@ -0,0 +1,3 @@
+#import os
+import django
+#os.system("start notepad.exe")
